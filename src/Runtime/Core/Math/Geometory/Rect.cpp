8205e376c9a683ae065c217d78f60024248f8165
Gajumaru314159 <igm6323@gmail.com>
2021-09-20 21:57:50 +0900

    修正続き

--- src/Runtime/Core/Math/Geometory/Rect.cpp
+++ src/Runtime/Core/Math/Geometory/Rect.cpp
@@ -8,21 +8,21 @@
 
 namespace ob {
 
-    //-----------------------------------------------------------------------------
+    //@―---------------------------------------------------------------------------
     //! @brief      コンストラクタ(点群指定)
     //! 
     //! @details    指定された点群をすべて含むボックスを構築する
-    //-----------------------------------------------------------------------------
-    Rect::Rect(const Vec2* points, size_t count) {
-        if (count == 0) {
+    //@―---------------------------------------------------------------------------
+    Rect::Rect(const gsl::span<Vec2>& points) {
+        if (points.size() <= 1) {
             Reset();
             return;
         }
         Vec2 min = points[0];
         Vec2 max = points[0];
-        for (s32 i = 1; i < count; i++) {
-            min = Vec2::Min(min, points[i]);
-            max = Vec2::Max(min, points[i]);
+        for (auto& point : points) {
+            min = Vec2::Min(min, point);
+            max = Vec2::Max(max, point);
         }
         center = (min + max) * 0.5f;
         size = max - min;
