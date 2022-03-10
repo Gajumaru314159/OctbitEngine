a53dffdcd7f8d30d4b16cbe4e0c996ec3dc4fbb8
Gajumaru314159 <igm6323@gmail.com>
2022-03-10 21:09:11 +0900

    Applicationの構成変更

---
+++ Sources/Application/Source.cpp
@@ -0,0 +1,11 @@
+﻿#include <Framework/Platform/Window/Window.h>
+using namespace ob;
+
+int main() {
+    
+    platform::WindowCreationDesc desc;
+    ob::platform::Window window(desc);
+    window.Show();
+    
+    return 0;
+}
