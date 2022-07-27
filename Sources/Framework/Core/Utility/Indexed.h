85d5af65df4b8a20c2967927ca275b34b2bffd0e
Gajumaru314159 <igm6323@gmail.com>
2022-07-27 23:37:01 +0900

    インデックス付きEnumlator追加

---
+++ Sources/Framework/Core/Utility/Indexed.h
@@ -0,0 +1,90 @@
+﻿//***********************************************************
+//! @file
+//! @brief		インデックス付きEnumlator
+//! @author		Gajumaru
+//***********************************************************
+#pragma once
+#include <Framework/Core/CorePrivate.h>
+
+namespace ob::core {
+
+    //@―---------------------------------------------------------------------------
+    //! @brief  インデックス付きEnumlator
+    //@―---------------------------------------------------------------------------
+	template <class Type, class TItr = decltype(std::begin(std::declval<Type>())), class = decltype(std::end(std::declval<Type>()))>
+	inline constexpr auto IndexedRef(Type&& iterable)
+	{
+		// Reference: http://reedbeta.com/blog/python-like-enumerate-in-cpp17/
+		// Reference: https://github.com/Siv3D/OpenSiv3D/blob/main/Siv3D/include/Siv3D/Indexed.hpp
+
+		struct Iterator
+		{
+			size_t index;
+			TItr it;
+			constexpr bool operator != (const Iterator& other) const { return it != other.it; }
+			constexpr void operator ++() { ++index; ++it; }
+			constexpr auto operator *() const { return std::tie(index, *it); }
+		};
+
+		struct IterableWrapper
+		{
+			Type iterable;
+			constexpr auto begin() { return Iterator{ 0, std::begin(iterable) }; }
+			constexpr auto end() { return Iterator{ 0, std::end(iterable) }; }
+		};
+
+		return IterableWrapper{ std::forward<Type>(iterable) };
+	}
+
+	//@―---------------------------------------------------------------------------
+	//! @brief		インデックス付きEnumlator
+	//! 
+	//! @details	使用方法
+	//!				```c++
+	//!				for(auto[index,item]:Indexed(container)){
+	//! 
+	//!				}
+	//!				```
+	//@―---------------------------------------------------------------------------
+	template <class Type, class TItr = decltype(std::cbegin(std::declval<Type>())), class = decltype(std::cend(std::declval<Type>()))>
+	inline constexpr auto Indexed(Type&& iterable)
+	{
+		return IndexedRef<Type, TItr>(std::forward<Type>(iterable));
+	}
+
+	//@―---------------------------------------------------------------------------
+	//! @brief  インデックス付き逆Enumlator
+	//@―---------------------------------------------------------------------------
+	template <class Type, class TItr = decltype(std::rbegin(std::declval<Type>())), class = decltype(std::rend(std::declval<Type>()))>
+	inline constexpr auto ReverseIndexedRef(Type&& iterable)
+	{
+		struct Iterator
+		{
+			size_t index;
+			TItr it;
+			constexpr bool operator != (const Iterator& other) const { return it != other.it; }
+			constexpr void operator ++() { --index; ++it; }
+			constexpr auto operator *() const { return std::tie(index, *it); }
+		};
+
+		struct IterableWrapper
+		{
+			Type iterable;
+			constexpr auto begin() { return Iterator{ std::size(iterable) - 1, std::rbegin(iterable) }; }
+			constexpr auto end() { return Iterator{ 0, std::rend(iterable) }; }
+		};
+
+		return IterableWrapper{ std::forward<Type>(iterable) };
+	}
+
+	//@―---------------------------------------------------------------------------
+	//! @brief  インデックス付き逆Enumlator
+	//@―---------------------------------------------------------------------------
+	template <class Type, class TItr = decltype(std::crbegin(std::declval<Type>())), class = decltype(std::crend(std::declval<Type>()))>
+	inline constexpr auto ReverseIndexed(Type&& iterable)
+	{
+		return ReverseIndexedRef<Type, TItr>(std::forward<Type>(iterable));
+	}
+
+
+}// namespcae ob
