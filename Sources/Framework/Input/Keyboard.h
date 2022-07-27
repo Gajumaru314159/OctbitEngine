836a3ad1e33173f90f62a564aa7bb18dea8ebdc2
Gajumaru314159 <igm6323@gmail.com>
2022-07-28 02:29:13 +0900

    キーボード入力

--- Sources/Framework/Input/Keyboard.h
+++ Sources/Framework/Input/Keyboard.h
@@ -13,12 +13,46 @@ namespace ob::input {
 
 	class Keyboard {
 	public:
-		static constexpr u32 Code{ OB_FNV32("Keyboard")};
+
+		static constexpr u32 Code{ OB_FNV32("Keyboard")};	//!< デバイスコード
+
+		//@―---------------------------------------------------------------------------
+		//! @brief      ボタン取得
+		//@―---------------------------------------------------------------------------
+		static constexpr Button GetButton(Key key) { return {Code,key}; }
+
 	public:
-		OB_DECL_BUTTON(W);
-		OB_DECL_BUTTON(S);
 		OB_DECL_BUTTON(A);
+		OB_DECL_BUTTON(B);
+		OB_DECL_BUTTON(C);
 		OB_DECL_BUTTON(D);
+		OB_DECL_BUTTON(E);
+		OB_DECL_BUTTON(F);
+		OB_DECL_BUTTON(G);
+		OB_DECL_BUTTON(H);
+		OB_DECL_BUTTON(I);
+		OB_DECL_BUTTON(J);
+		OB_DECL_BUTTON(K);
+		OB_DECL_BUTTON(L);
+		OB_DECL_BUTTON(M);
+		OB_DECL_BUTTON(N);
+		OB_DECL_BUTTON(O);
+		OB_DECL_BUTTON(P);
+		OB_DECL_BUTTON(Q);
+		OB_DECL_BUTTON(R);
+		OB_DECL_BUTTON(S);
+		OB_DECL_BUTTON(T);
+		OB_DECL_BUTTON(U);
+		OB_DECL_BUTTON(V);
+		OB_DECL_BUTTON(W);
+		OB_DECL_BUTTON(X);
+		OB_DECL_BUTTON(Y);
+		OB_DECL_BUTTON(Z);
+		OB_DECL_BUTTON(LeftArrow);
+		OB_DECL_BUTTON(RightArrow);
+		OB_DECL_BUTTON(UpArrow);
+		OB_DECL_BUTTON(DownArrow);
+		OB_DECL_BUTTON(Escape);
 	};
 
 #undef OB_DECL_BUTTON
