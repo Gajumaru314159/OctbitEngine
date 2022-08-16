e76cba474e657845ccaaa45fad46575f6a40c732
Gajumaru314159 <igm6323@gmail.com>
2022-08-17 02:40:03 +0900

    Name実装

---
+++ Source/Private/Framework/Core/String/NameDictionary.cpp
@@ -0,0 +1,71 @@
+﻿//***********************************************************
+//! @file
+//! @brief		識別用名辞書
+//! @author		Gajumaru
+//***********************************************************
+#include <Framework/Core/String/NameDictionary.h>
+#include <Framework/Core/Misc/CRCHash.h>
+
+namespace ob::core{
+    
+    //@―---------------------------------------------------------------------------
+    //! @brief      Nameを作成して辞書に追加
+    //! 
+    //! @details    文字列に対してユニークなハッシュ値を割り当てます。
+    //@―---------------------------------------------------------------------------
+    Name NameDictionary::makeName(StringView nameSV) {
+
+        if (nameSV.empty())return Name();
+
+        // ハッシュ値計算
+        auto hash = CRCHash::Compute(nameSV.data(), nameSV.size());
+
+        // 追加済みかチェック
+        // ※重複でハッシュが違うものは後で検出
+        Name name = findName(hash);
+        if (name.toSV() == nameSV) {
+            return std::move(name);
+        }
+
+        // 追加
+        ScopeLock lock(m_lock);
+        
+        while (true) {
+
+            auto itr = m_dict.find(hash);
+
+            // 新規追加
+            if (itr == m_dict.end()) {
+                auto& item = m_dict.emplace(hash, detail::NameData(nameSV, hash));
+                return Name(item.second);
+            }
+
+            // 追加済み(Hashは変更済み)
+            if (itr->second.getName() == nameSV) {
+                return Name(itr->second);
+            }
+
+            // 同じハッシュの別名がある場合はハッシュ変更
+            {
+                hash++;
+            }
+
+        }
+
+    }
+
+    //@―---------------------------------------------------------------------------
+    //! @brief      ハッシュ値からNameを取得
+    //! 
+    //! @details    見つからない場合は空の Name を返します。
+    //@―---------------------------------------------------------------------------
+    Name NameDictionary::findName(Name::Hash hash)const {
+        ScopeLock lock(m_lock);
+        auto itr = m_dict.find(hash);
+        if (itr == m_dict.end()) {
+            return Name();
+        }
+        return Name(itr->second);
+    }
+
+}// namespace ob::core
---
+++ Source/Private/Framework/Core/String/NameDictionary.h
@@ -0,0 +1,64 @@
+﻿//***********************************************************
+//! @file
+//! @brief		識別用名辞書
+//! @author		Gajumaru
+//***********************************************************
+#pragma once
+#include <Framework/Singleton/Singleton.h>
+#include <Framework/Core/CorePrivate.h>
+#include <Framework/Core/String/Name.h>
+#include <Framework/Core/Thread/SpinLock.h>
+
+namespace ob::core {
+
+    //! @cond
+    namespace detail{
+
+        class NameData {
+        public:
+            using Hash = Name::Hash;
+        public:
+            NameData(StringView name, Hash hash) 
+                : m_name(name)
+                , m_hash(hash)
+            {
+            }
+            StringView getName()const { return m_name; }
+            Hash getHash()const { return m_hash; }
+        private:
+            String m_name;
+            Hash m_hash;
+        };
+
+    }
+    //! @endcond
+
+
+    //@―---------------------------------------------------------------------------
+    //! @brief  識別用名辞書
+    //@―---------------------------------------------------------------------------
+    class NameDictionary:public Singleton<NameDictionary> {
+    public:
+
+        //@―---------------------------------------------------------------------------
+        //! @brief      Nameを作成して辞書に追加
+        //! 
+        //! @details    文字列に対してユニークなハッシュ値を割り当てます。
+        //@―---------------------------------------------------------------------------
+        Name makeName(StringView name);
+
+        //@―---------------------------------------------------------------------------
+        //! @brief      ハッシュ値からNameを取得
+        //! 
+        //! @details    見つからない場合は空の Name を返します。
+        //@―---------------------------------------------------------------------------
+        Name findName(Name::Hash hash)const;
+
+    private:
+
+        SpinLock m_lock;
+        HashMap<Name::Hash, detail::NameData> m_dict;
+
+    };
+
+}// namespcae ob::core
