028cfb490c3d35d372806c304e4601c7ef996d0d
Gajumaru314159 <igm6323@gmail.com>
2022-09-05 02:13:04 +0900

    テスト追加

---
+++ Source/Test/Framework/Platform/Source/Box.cpp
@@ -0,0 +1,60 @@
+﻿//***********************************************************
+//! @file
+//! @brief		Box のテスト
+//! @author		Gajumaru
+//***********************************************************
+#include <Framework/Platform/Window.h>
+
+using namespace ob;
+
+TEST(Window, ShowHide) {
+
+    using namespace ob::platform;
+    WindowDesc desc;
+    Window window = Window(desc);
+
+    window.show();
+    window.moveToCenter();
+
+    window.maximize();
+    ASSERT_TRUE(window.getState().has(WindowState::Maximized));
+
+    window.minimize();
+    ASSERT_TRUE(window.getState().has(WindowState::Minimized));
+
+    window.restoreSize();
+
+}
+
+TEST(Window, Title) {
+
+    using namespace ob::platform;
+    WindowDesc desc;
+    desc.title = TC("AAA");
+    Window window = Window(desc);
+    ASSERT_EQ(window.getTitle(), TC("AAA"));
+
+    window.setTitle(TC("BBB"));
+    ASSERT_EQ(window.getTitle(), TC("BBB"));
+
+}
+
+TEST(Window, PosSize) {
+
+    using namespace ob::platform;
+    WindowDesc desc;
+    desc.clientSize = Size(512, 256);
+    Window window = Window(desc);
+    ASSERT_EQ(window.getSize(), Size(512,256));
+
+
+    window.setPosition({ 123,250 });
+    ASSERT_EQ(window.getPosition(), Point(123, 250));
+
+    window.setPosition({ 456,250 });
+    ASSERT_EQ(window.getPosition(), Point(456, 250));
+
+    //window.setSize(1024,256);
+    //ASSERT_EQ(window.getSize(), Size(1024, 256));
+
+}
---
+++ Source/Test/Framework/Platform/Source/main.cpp
@@ -0,0 +1,16 @@
+﻿//***********************************************************
+//! @file
+//! @brief		エントリー
+//! @author		Gajumaru
+//***********************************************************
+#include <Framework/Platform/System/PlatformSystem.h>
+
+int main(int argc, char** argv) {
+
+    // 依存関係のあるシステムを初期化
+    ob::Logger logger;
+    ob::platform::PlatformSystem::Instance();
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
