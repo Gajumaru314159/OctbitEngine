edf54c2d32978d562e1a7b52fa1a4c0886ae098a
Gajumaru314159 <igm6323@gmail.com>
2022-09-18 21:54:35 +0900

    Graphicインクルード追加

---
+++ Source/Framework/Graphic/Public/Framework/Graphic/All.h
@@ -0,0 +1,19 @@
+﻿//***********************************************************
+//! @file
+//! @brief		Graphicインクルード
+//! @author		Gajumaru
+//***********************************************************
+#pragma once
+#include <Framework/Graphic/Buffer.h>
+#include <Framework/Graphic/CommandList.h>
+#include <Framework/Graphic/Constants.h>
+#include <Framework/Graphic/DescriptorTable.h>
+#include <Framework/Graphic/Device.h>
+#include <Framework/Graphic/PipelineState.h>
+#include <Framework/Graphic/RenderTarget.h>
+#include <Framework/Graphic/ResourceBarrier.h>
+#include <Framework/Graphic/RootSignature.h>
+#include <Framework/Graphic/Shader.h>
+#include <Framework/Graphic/SwapChain.h>
+#include <Framework/Graphic/System.h>
+#include <Framework/Graphic/Texture.h>
