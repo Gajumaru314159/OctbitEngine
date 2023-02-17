2933d2b8522613a403cee0df2ee2868fa361dd7d
Gajumaru314159 <igm6323@gmail.com>
2023-02-18 01:03:11 +0900

    BlobのStream初期化

---
+++ Source/Framework/Core/Private/Framework/Core/Misc/Blob.cpp
@@ -0,0 +1,23 @@
+﻿//***********************************************************
+//! @file
+//! @brief Blob
+//! @author Gajumaru
+//***********************************************************
+#include <Framework/Core/Misc/Blob.h>
+#include <Framework/Core/File/Stream.h>
+
+namespace ob::core {
+
+    //@―---------------------------------------------------------------------------
+    //! @brief ストリームから生成
+    //@―---------------------------------------------------------------------------
+    Blob::Blob(Stream& stream) {
+
+        if (stream.canRead()) {
+            resize(stream.size());
+            stream.read(data(), size());
+        }
+
+    }
+
+} // namespace ob
