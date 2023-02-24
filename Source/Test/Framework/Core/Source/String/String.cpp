cc408332af26df287374c677724fee08bf3cd7ba
Gajumaru314159 <igm6323@gmail.com>
2023-02-24 22:56:35 +0900

    テスト削除

--- Source/Test/Framework/Core/Source/String/String.cpp
+++ Source/Test/Framework/Core/Source/String/String.cpp
@@ -10,12 +10,5 @@ using namespace ob;
 
 TEST(String, Construct) {
 
-    // サイズ / 中心
-    {
-        String text(TC("Text"));
-
-        utf8::utf16to8(text);
-        ASSERT_EQ(text.size(), 4);
-    }
 
 }
