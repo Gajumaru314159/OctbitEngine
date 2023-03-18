156791efe3f7fdc59f6eadd1b43b51cc0894a58c
Gajumaru314159 <igm6323@gmail.com>
2023-03-19 04:05:34 +0900

    コメント訂正

--- Source/Framework/Input/Private/Framework/Input/Button.cpp
+++ Source/Framework/Input/Private/Framework/Input/Button.cpp
@@ -11,7 +11,7 @@
 namespace ob::input {
 
 	//@―---------------------------------------------------------------------------
-	//! @berif	状態が state か
+	//! @brief	状態が state か
 	//@―---------------------------------------------------------------------------
 	bool Button::is(ButtonState state)const {
 		if (auto device = GEngine->ref<InputModule>().findDevice(m_deviceId, m_user)) {
@@ -22,35 +22,35 @@ namespace ob::input {
 	}
 
 	//@―---------------------------------------------------------------------------
-	//! @berif	押されているか
+	//! @brief	押された瞬間か
 	//@―---------------------------------------------------------------------------
 	bool Button::down()const {
 		return is(ButtonState::Down);
 	}
 
 	//@―---------------------------------------------------------------------------
-	//! @berif	離されているか
+	//! @brief	離された瞬間か
 	//@―---------------------------------------------------------------------------
 	bool Button::up()const {
 		return is(ButtonState::Up);
 	}
 
 	//@―---------------------------------------------------------------------------
-	//! @berif	押された瞬間か
+	//! @brief	押されているか
 	//@―---------------------------------------------------------------------------
 	bool Button::pressed()const {
 		return is(ButtonState::Pressed);
 	}
 
 	//@―---------------------------------------------------------------------------
-	//! @berif	離された瞬間か
+	//! @brief	離されているか
 	//@―---------------------------------------------------------------------------
 	bool Button::released()const {
 		return is(ButtonState::Released);
 	}
 
 	//@―---------------------------------------------------------------------------
-	//! @berif	バインド
+	//! @brief	バインド
 	//@―---------------------------------------------------------------------------
 	bool Button::bindDown(ButtonHandle& handle, const ButtonDelegate& func)const {
 		return bind(ButtonState::Down, handle, func);
@@ -66,7 +66,7 @@ namespace ob::input {
 	}
 
 	//@―---------------------------------------------------------------------------
-	//! @berif	バインド
+	//! @brief	バインド
 	//@―---------------------------------------------------------------------------
 	bool Button::bind(ButtonState state, ButtonHandle& handle, const ButtonDelegate& func)const {
 		if (auto device = GEngine->ref<InputModule>().findDevice(m_deviceId, m_user)) {
