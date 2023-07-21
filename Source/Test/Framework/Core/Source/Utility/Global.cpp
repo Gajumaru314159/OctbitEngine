2ba9a2a740001c549546287aa5f328c6232deffa
Gajumaru314159 <igm6323@gmail.com>
2023-07-22 01:03:44 +0900

    Global追加

---
+++ Source/Test/Framework/Core/Source/Utility/Global.cpp
@@ -0,0 +1,59 @@
+﻿//***********************************************************
+//! @file
+//! @brief		Path のテスト
+//! @author		Gajumaru
+//***********************************************************
+#include <Framework/Core/Core.h>
+#include <Framework/Core/Utility/Global.h>
+
+using namespace ob;
+
+namespace test {
+
+    class TestSystemA {
+    public:
+        TestSystemA() {
+
+        }
+        void func() {
+            LOG_INFO("TestSystemA");
+        }
+    };
+
+    class TestSystemB {
+    public:
+        TestSystemB(s32 value) {
+            m_value = value;
+        }
+        void func() {
+            LOG_INFO("TestSystemB : {}",m_value);
+        }
+    private:
+        s32 m_value;
+    };
+
+}
+
+TEST(Global, Global)
+{
+    if (Global<test::TestSystemA>::Instance() == nullptr) {
+        LOG_INFO("TestSystemAはインスタン化されていません");
+    }
+    {
+        Global<test::TestSystemA> test;
+        if (auto system = Global<test::TestSystemA>::Instance()) {
+            system->func();
+        }
+    }
+    if (Global<test::TestSystemA>::Instance() == nullptr) {
+        LOG_INFO("TestSystemAはインスタン化されていません");
+    }
+    {
+        if (auto system = Global<test::TestSystemA>::Instance()) {
+            system->func();
+        }
+    }
+    if (Global<test::TestSystemB>::Instance() == nullptr) {
+        LOG_INFO("TestSystemBはインスタン化されていません");
+    }
+}
