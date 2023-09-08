a64067b769741a0cfc65c69a901b7beab67684bd
Gajumaru314159 <igm6323@gmail.com>
2023-09-09 03:17:29 +0900

    不要なログ初期化を削除

--- Source/Test/Framework/Core/Source/main.cpp
+++ Source/Test/Framework/Core/Source/main.cpp
@@ -4,10 +4,6 @@
 //! @author		Gajumaru
 //***********************************************************
 int main(int argc, char** argv) {
-
-    // 依存関係のあるシステムを初期化
-    //ob::Logger logger;
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
--- Source/Test/Framework/Platform/Source/main.cpp
+++ Source/Test/Framework/Platform/Source/main.cpp
@@ -6,10 +6,6 @@
 #include <Framework/Platform/System.h>
 
 int main(int argc, char** argv) {
-
-    // 依存関係のあるシステムを初期化
-    ob::Logger logger;
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
