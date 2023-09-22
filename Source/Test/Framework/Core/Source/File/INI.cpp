c100304efbcbf492104848ddaaeae07460f47163
Gajumaru314159 <igm6323@gmail.com>
2023-09-23 04:03:29 +0900

    INIを実装

---
+++ Source/Test/Framework/Core/Source/File/INI.cpp
@@ -0,0 +1,35 @@
+﻿//***********************************************************
+//! @file
+//! @brief		Path のテスト
+//! @author		Gajumaru
+//***********************************************************
+#include <Framework/Core/File/INI.h>
+#include <filesystem>
+
+using namespace ob;
+
+TEST(INI, Test)
+{
+    auto filename = "engine.ini";
+    std::filesystem::remove(filename);
+
+    {
+        INI ini(filename);
+        EXPECT_TRUE(ini.empty());
+
+        EXPECT_TRUE(ini["common"]["platform"].empty());
+        ini["common"]["platform"] = "windows";
+        EXPECT_EQ(ini["common"]["platform"],"windows");
+
+        ini.save();
+    }
+
+    {
+        INI ini;
+        if (ini.load(filename)) {
+
+            EXPECT_EQ(ini["common"]["platform"], "windows");
+
+        }
+    }
+}
