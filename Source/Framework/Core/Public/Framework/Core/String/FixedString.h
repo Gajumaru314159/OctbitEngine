577a1c1420abed28a5371b661d05ce05751c20b2
Gajumaru314159 <igm6323@gmail.com>
2024-07-07 11:19:27 +0900

    FixedString実装

---
+++ Source/Framework/Core/Public/Framework/Core/String/FixedString.h
@@ -0,0 +1,47 @@
+﻿//***********************************************************
+//! @file
+//! @brief		固定長文字列
+//! @author		Gajumaru
+//***********************************************************
+#pragma once
+#include <string>
+#include <ostream>
+#include <Framework/Core/CoreTypes.h>
+#include <Framework/Core/String/StringView.h>
+
+namespace ob::core {
+
+	//@―---------------------------------------------------------------------------
+	//! @brief		固定長文字列基底
+	//@―---------------------------------------------------------------------------
+	template<class TChar, size_t N>
+	class FixedStringBase {
+		static_assert(0<N);
+    public:
+        using value_type = TChar;
+        using pointer = TChar*;
+        using reference = TChar&;
+        using difference_type = s64;
+
+		FixedStringBase() {}
+		FixedStringBase(StringViewBase<TChar> str) {
+			size_t len = std::min(N - 1, str.size());
+			memcpy_s(m_data, N, str.data(), len);
+			m_data[len + 1] = (TChar)'\0';
+		}
+
+		void push_back(TChar c) {
+			if (N <= m_size + 1)return;
+			m_data[m_size] = c;
+			m_size++;
+			m_data[m_size] = 0;
+		}
+	private:
+		TChar m_data[N] = {};
+		size_t m_size = 0;
+	};
+
+	template<size_t N>
+	using FixedString = FixedStringBase<Char,N>;
+
+}
