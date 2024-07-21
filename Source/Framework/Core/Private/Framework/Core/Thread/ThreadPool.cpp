338b48648e7f53ecfdf75b689bedf7445577eac1
Gajumaru314159 <igm6323@gmail.com>
2024-07-21 20:47:12 +0900

    ThreadPool実装

---
+++ Source/Framework/Core/Private/Framework/Core/Thread/ThreadPool.cpp
@@ -0,0 +1,68 @@
+﻿//***********************************************************
+//! @file
+//! @brief		スレッド
+//! @author		Gajumaru
+//***********************************************************
+#pragma once
+#include <Framework/Core/Thread/ThreadPool.h>
+
+namespace ob::core {
+
+	ThreadPool::ThreadPool() {
+		resize(4);
+	}
+
+	ThreadPool::~ThreadPool() {
+		join();
+	}
+
+	void ThreadPool::resize(s32 threadNum) {
+
+		// 既存のスレッド終了
+		join();
+
+		// スレッド数の変更
+		m_threads.clear();
+		m_running = true;
+		for (s32 i = 0; i < threadNum; ++i) {
+			m_threads.emplace_back(Format("ThreadPool_{:2}", i), ThreadPriority::Level3, [this] { process(); });
+		}
+
+	}
+
+	void ThreadPool::enqueue(Action&& action) {
+		{
+			std::lock_guard lock(m_mutex);
+			m_queue.emplace_back(std::move(action));
+		}
+		m_condition.notify_one();
+	}
+
+	void ThreadPool::join() {
+		{
+			std::lock_guard lock(m_mutex);
+			m_running = false;
+			m_condition.notify_all();
+		}
+	}
+
+	void ThreadPool::process() {
+		while (true) {
+			Action action;
+			{
+				std::unique_lock lock(m_mutex);
+				m_condition.wait(lock, [this] { return !m_queue.empty() || !m_running; });
+
+				if (m_running == false) {
+					break;
+				}
+
+				action = std::move(m_queue.back());
+				m_queue.pop_back();
+			}
+			// 処理
+			action();
+		}
+	}
+
+}
---
+++ Source/Framework/Core/Public/Framework/Core/Thread/ThreadPool.h
@@ -0,0 +1,38 @@
+﻿//***********************************************************
+//! @file
+//! @brief		スレッド
+//! @author		Gajumaru
+//***********************************************************
+#pragma once
+#include <Framework/Core/CorePrivate.h>
+#include <Framework/Core/String/String.h>
+#include <Framework/Core/Thread/Thread.h>
+#include <Framework/Core/Utility/Noncopyable.h>
+#include <Framework/Core/Utility/Singleton.h>
+
+namespace ob::core {
+
+	class ThreadPool : public Singleton<ThreadPool> {
+	public:
+
+		ThreadPool();
+		~ThreadPool();
+
+		void resize(s32 threadNum);
+		void enqueue(Action&& action);
+
+	private:
+
+		void join();
+		void process();
+
+	private:
+		std::mutex m_mutex;
+		std::condition_variable m_condition;
+		std::atomic_bool m_running = false;
+		Deque<Action> m_queue;
+
+		Vector<Pimpl<Thread>> m_threads;
+	};
+
+}
