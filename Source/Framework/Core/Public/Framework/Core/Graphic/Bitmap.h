c182a1fe95fd686f31a20207dd4c64e2805a3fbf
Gajumaru314159 <igm6323@gmail.com>
2024-07-21 14:05:41 +0900

    Bitmap実装

--- Source/Framework/Core/Public/Framework/Core/Graphic/Bitmap.h
+++ Source/Framework/Core/Public/Framework/Core/Graphic/Bitmap.h
@@ -1,6 +1,6 @@
 ﻿//***********************************************************
 //! @file
-//! @brief		ファイル説明
+//! @brief		Bitmap
 //! @author		Gajumaru
 //***********************************************************
 #pragma once
@@ -22,12 +22,13 @@ namespace ob::core {
         PNG,
     };
 
+    
     //@―---------------------------------------------------------------------------
-    //! @brief  説明
+    //! @brief  ビットマップ
     //@―---------------------------------------------------------------------------
     class Bitmap {
     public:
-        using base_type                 = Vector<Color>;                        //!< ベース型
+        using base_type                 = Vector<IntColor>;                     //!< ベース型
         using iterator                  = base_type::iterator;                  //!< イテレータ
         using const_iterator            = base_type::const_iterator;            //!< const イテレータ
         using reverse_iterator          = base_type::reverse_iterator;          //!< 逆イテレータ
@@ -38,39 +39,56 @@ namespace ob::core {
         // コンストラクタ / デストラクタ
         //===============================================================
 
-        //@―---------------------------------------------------------------------------
-        //! @brief  説明
-        //@―---------------------------------------------------------------------------
         Bitmap() = default;
-        Bitmap(s32 width, s32 height, IntColor color = IntColor::White);
+        Bitmap(s32 width, s32 height, IntColor color = IntColor::Black);
         template<class TGen>
         Bitmap(s32 width, s32 height,TGen generator);
         Bitmap(StringView path,ImageFormat fotrmat = ImageFormat::Unspecified);
 
+
+        //===============================================================
+        // ゲッター
+        //===============================================================
+
         s32 width()const noexcept;
         s32 height()const noexcept;
         Size size()const noexcept;
-
-        s32 stribe()const noexcept;
-
-        s32 pixelCount()const noexcept;
+        s32 stride()const noexcept;
+        size_t pixelCount()const noexcept;
         size_t byteSize()const noexcept;
-
         bool empty()const noexcept;
+
+
+        //===============================================================
+        // 領域
+        //===============================================================
+
         void shrink_to_fit();
         void clear();
+
+
+        //===============================================================
+        // データアクセス
+        //===============================================================
 
         IntColor* operator[](size_t y);
         const IntColor* operator[](size_t y)const;
 
+        IntColor& at(s32 x, s32 y);
+        const IntColor& at(s32 x,s32 y)const;
 
         IntColor* data();
         const IntColor* data()const;
 
+
+        //===============================================================
+        // 加工
+        //===============================================================
+
         void fill(IntColor color);
 
-        void resize(s32 width, s32 height);
-        void resize(Size size);
+        void resize(s32 width, s32 height, IntColor color = IntColor::Black);
+        void resize(Size size, IntColor color = IntColor::Black);
 
 
         void flipX();
@@ -84,28 +102,23 @@ namespace ob::core {
         template<class TFunc>
         const Bitmap& forEach(TFunc func)const;
 
-        iterator begin() noexcept;
-        iterator end() noexcept;
-        const_iterator begin() const noexcept;
-        const_iterator end() const noexcept;
-        const_iterator cbegin() const noexcept;
-        const_iterator cend() const noexcept;
-        reverse_iterator rbegin() noexcept;
-        reverse_iterator rend() noexcept;
-        const_reverse_iterator rbegin() const noexcept;
-        const_reverse_iterator rend() const noexcept;
-        const_reverse_iterator crbegin() const noexcept;
-        const_reverse_iterator crend() const noexcept;
 
+        //===============================================================
+        // イテレータ
+        //===============================================================
 
-        // セーブ
-        
-        // フィルタ
-
-        // 回転
-
-        // ポリゴン化
-
+        iterator begin() noexcept { return m_pixels.begin(); }
+        iterator end() noexcept { return m_pixels.end(); }
+        const_iterator begin() const noexcept { return m_pixels.begin(); }
+        const_iterator end() const noexcept { return m_pixels.end(); }
+        const_iterator cbegin() const noexcept { return m_pixels.cbegin(); }
+        const_iterator cend() const noexcept { return m_pixels.cend(); }
+        reverse_iterator rbegin() noexcept { return m_pixels.rbegin(); }
+        reverse_iterator rend() noexcept { return m_pixels.rend(); }
+        const_reverse_iterator rbegin() const noexcept { return m_pixels.rbegin(); }
+        const_reverse_iterator rend() const noexcept { return m_pixels.rend(); }
+        const_reverse_iterator crbegin() const noexcept { return m_pixels.crbegin(); }
+        const_reverse_iterator crend() const noexcept { return m_pixels.crend(); }
 
     private:
 
@@ -117,14 +130,34 @@ namespace ob::core {
 
 
 
+    template<class TGen>
+    Bitmap::Bitmap(s32 width, s32 height, TGen generator) {
+        resize(width, height);
+		forEach(generator);
+    }
 
+    template<class TFunc>
+    Bitmap& Bitmap::forEach(TFunc func) {
+        size_t index = 0;
+        for(s32 y = 0; y < m_size.height; ++y) {
+			for(s32 x = 0; x < m_size.width; ++x) {
+				func(m_pixels[index],x,y);
+                index++;
+			}
+		}
+        return *this;
+    }
 
-    //===============================================================
-    // インライン関数
-    //===============================================================
-    //! @cond
+    template<class TFunc>
+    const Bitmap& Bitmap::forEach(TFunc func)const {
+        size_t index = 0;
+        for (s32 y = 0; y < m_size.height; ++y) {
+            for (s32 x = 0; x < m_size.width; ++x) {
+                func(m_pixels[index], x, y);
+                index++;
+            }
+        }
+        return *this;
+    }
 
-
-
-    //! @endcond
 }
