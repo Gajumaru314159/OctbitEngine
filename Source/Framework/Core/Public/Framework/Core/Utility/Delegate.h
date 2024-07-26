6c26dc7137cae951030cd76ab3b04408bbc4908d
Gajumaru314159 <igm6323@gmail.com>
2024-07-27 02:19:30 +0900

    MiniDelegateを実装

---
+++ Source/Framework/Core/Public/Framework/Core/Utility/Delegate.h
@@ -0,0 +1,159 @@
+﻿//***********************************************************
+//! @file
+//! @brief		ミニデリゲート
+//! @author		Gajumaru
+//***********************************************************
+#pragma once
+#include <cassert>
+
+namespace ob::core {
+
+	//@ cond
+	template<class>
+	class MiniDalegate;
+	//@ endcond
+
+    //@―---------------------------------------------------------------------------
+//! @brief      Ownerのみをキャプチャできる軽量デリゲート
+//!	@details    通常のデリゲートよりも軽量ですが、ラムダ式のように変数をキャプチャすることはできません。
+//@―---------------------------------------------------------------------------
+    template<class R, class... Args>
+    class MiniDalegate<R(Args...)> {
+    public:
+        using StaticFuncPtr = R(*)(Args...);
+        constexpr static size_t kBufferSize = 24; // T (8) + BaseInvoker (8) + Invoker (8) = 24
+
+    public:
+        //@―---------------------------------------------------------------------------
+        //! @brief      デフォルトコンストラクタ
+        //! @details    MiniDalegateクラスのデフォルトコンストラクタです。
+        //@―---------------------------------------------------------------------------
+        MiniDalegate() = default;
+
+        //@―---------------------------------------------------------------------------
+        //! @brief      コンストラクタ
+        //! @param[in]  func  静的関数ポインタ
+        //@―---------------------------------------------------------------------------
+        MiniDalegate(StaticFuncPtr func) {
+            assign(func);
+        }
+
+        //@―---------------------------------------------------------------------------
+        //! @brief      コンストラクタ
+        //! @param[in]  object  オブジェクトの参照
+        //! @param[in]  func    メンバ関数ポインタ
+        //@―---------------------------------------------------------------------------
+        template<class T>
+        MiniDalegate(T& object, R(T::* func)(Args...)) {
+            assign(object, func);
+        }
+
+        //@―---------------------------------------------------------------------------
+        //! @brief      コンストラクタ
+        //! @param[in]  object  オブジェクトの参照
+        //! @param[in]  func    メンバ関数ポインタ(const)
+        //@―---------------------------------------------------------------------------
+        template<class T>
+        MiniDalegate(const T& object, R(T::* func)(Args...) const) {
+            assign(object, func);
+        }
+
+        //@―---------------------------------------------------------------------------
+        //! @brief      静的関数ポインタを割り当てる
+        //! @param[in]  func  静的関数ポインタ
+        //@―---------------------------------------------------------------------------
+        void assign(StaticFuncPtr func) {
+            static_assert(sizeof(StaticInvoker) <= kBufferSize, "buffer is too small");
+            reset();
+            new (m_buffer.data()) StaticInvoker(func);
+        }
+
+        //@―---------------------------------------------------------------------------
+        //! @brief      オブジェクトとメンバ関数ポインタを割り当てる
+        //! @param[in]  object  オブジェクトの参照
+        //! @param[in]  func    メンバ関数ポインタ
+        //@―---------------------------------------------------------------------------
+        template<class T>
+        void assign(T& object, R(T::* func)(Args...)) {
+            static_assert(sizeof(Invoker<T>) <= kBufferSize, "buffer is too small");
+            reset();
+            new (m_buffer.data()) Invoker(object, func);
+        }
+
+        //@―---------------------------------------------------------------------------
+        //! @brief      オブジェクトとメンバ関数ポインタを割り当てる(const)
+        //! @param[in]  object  オブジェクトの参照
+        //! @param[in]  func    メンバ関数ポインタ
+        //@―---------------------------------------------------------------------------
+        template<class T>
+        void assign(const T& object, R(T::* func)(Args...) const) {
+            static_assert(sizeof(ConstInvoker<T>) <= kBufferSize, "buffer is too small");
+            reset();
+            new (m_buffer.data()) ConstInvoker(object, func);
+        }
+
+        //@―---------------------------------------------------------------------------
+        //! @brief      デリゲートが有効かどうかを判定する
+        //! @return     デリゲートが有効な場合はtrue、そうでない場合はfalse
+        //@―---------------------------------------------------------------------------
+        operator bool() const {
+            return std::equal_range(m_buffer.begin(), m_buffer.end(), 0);
+        }
+
+        //@―---------------------------------------------------------------------------
+        //! @brief      デリゲートをリセットする
+        //@―---------------------------------------------------------------------------
+        void reset() {
+            m_buffer.fill(0);
+        }
+
+        //@―---------------------------------------------------------------------------
+        //! @brief      デリゲートを呼び出す
+        //! @param[in]  args  関数引数
+        //! @return     関数の戻り値
+        //@―---------------------------------------------------------------------------
+        R operator()(Args... args) const {
+            return reinterpret_cast<const BaseInvoker*>(m_buffer.data())->invoke(args...);
+        }
+
+    private:
+
+        struct BaseInvoker {
+            virtual R invoke(Args... args) const = 0;
+        };
+
+        struct StaticInvoker : public BaseInvoker {
+            StaticFuncPtr m_func;
+            StaticInvoker(StaticFuncPtr func) : m_func(func) {}
+            R invoke(Args... args) const {
+                return m_func(args...);
+            }
+        };
+
+        template<class T>
+        struct Invoker : public BaseInvoker {
+            using SIG = R(T::*)(Args...);
+            T& m_owner;
+            SIG m_method;
+            Invoker(T& owner, SIG method) : m_owner(owner), m_method(method) {}
+            R invoke(Args... args) const {
+                return (m_owner.*m_method)(args...);
+            }
+        };
+
+        template<class T>
+        struct ConstInvoker : public BaseInvoker {
+            using SIG = R(T::*)(Args...) const;
+            const T& m_owner;
+            SIG m_method;
+            ConstInvoker(const T& owner, SIG method) : m_owner(owner), m_method(method) {}
+            R invoke(Args... args) const {
+                return (m_owner.*m_method)(args...);
+            }
+        };
+
+    private:
+        Array<size_t, kBufferSize / sizeof(size_t)> m_buffer;
+    };
+
+}
---
+++ Source/Test/Framework/Core/Source/Utility/Delegate.cpp
@@ -0,0 +1,124 @@
+﻿//***********************************************************
+//! @file
+//! @brief		Delegate のテスト
+//! @author		Gajumaru
+//***********************************************************
+#include <Framework/Core/Template/Container/BitSet.h>
+#include <Framework/Core/Utility/Delegate.h>
+
+using namespace ob;
+
+BitSet<4> s_result;
+
+struct A {
+public:
+
+	A() {
+
+		MiniDalegate<void()> d00(*this, &A::func00);
+		MiniDalegate<bool()> d01(*this, &A::func01);
+		MiniDalegate<void(bool)> d02(*this, &A::func02);
+		MiniDalegate<bool(bool)> d03(*this, &A::func03);
+
+		d00();
+		d01();
+		d02(true);
+		d03(true);
+		
+		MiniDalegate<void()> d10(*this, &A::func10);
+		MiniDalegate<bool()> d11(*this, &A::func11);
+		MiniDalegate<void(bool)> d12(*this, &A::func12);
+		MiniDalegate<bool(bool)> d13(*this, &A::func13);
+
+		d10();
+		d11();
+		d12(true);
+		d13(true);
+		
+		MiniDalegate<void()> d20(&A::func20);
+		MiniDalegate<bool()> d21(&A::func21);
+		MiniDalegate<void(bool)> d22(&A::func22);
+		MiniDalegate<bool(bool)> d23(&A::func23);
+
+		d20();
+		d21();
+		d22(true);
+		d23(true);
+
+		sizeof(MiniDalegate<void()>);
+	}
+
+	void constTest() {		
+		MiniDalegate<void()> d10(*this, &A::func00);
+		MiniDalegate<bool()> d11(*this, &A::func01);
+		MiniDalegate<void(bool)> d12(*this, &A::func02);
+		MiniDalegate<bool(bool)> d13(*this, &A::func03);
+	}
+
+	void func00() {
+		m_result[0] = true;
+	}
+
+	bool func01() {
+		m_result[1] = true;
+		return true;
+	}
+
+	void func02(bool a) {
+		m_result[2] = true;
+	}
+
+	bool func03(bool a) {
+		m_result[3] = true;
+		return true;
+	}
+
+	void func10() const{
+		m_result[4] = true;
+	}
+
+	bool func11() const {
+		m_result[5] = true;
+		return true;
+	}
+
+	void func12(bool a) const {
+		m_result[6] = true;
+	}
+
+	bool func13(bool a) const {
+		m_result[7] = true;
+		return true;
+	}
+
+	static void func20() {
+		s_result[0] = true;
+	}
+
+	static bool func21() {
+		s_result[1] = true;
+		return true;
+	}
+
+	static void func22(bool a) {
+		s_result[2] = true;
+	}
+
+	static bool func23(bool a) {
+		s_result[3] = true;
+		return true;
+	}
+public:
+	bool success() {
+		return m_result.all() && s_result.all();
+	}
+private:
+	mutable BitSet<8> m_result;
+};
+
+TEST(MiniDelegate, Test) {
+
+	A a;
+	EXPECT_TRUE(a.success());
+
+}
