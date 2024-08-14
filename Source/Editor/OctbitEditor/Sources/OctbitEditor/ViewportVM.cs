10ad82a93d0db1d2b4802d8143f7618fee258323
Gajumaru314159 <igm6323@gmail.com>
2024-08-15 01:35:47 +0900

    アイコン追加

--- Source/Editor/OctbitEditor/Sources/OctbitEditor/ViewportVM.cs
+++ Source/Editor/OctbitEditor/Sources/OctbitEditor/ViewportVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reactive.Bindings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace OctbitEditor
 {
     public class ViewportVM
     {
+        public ViewportVM()
+        {
+
+        }
+
+
+        public ReactivePropertySlim<bool> ToolIsEnabled { get; } = new (true);
+        public ReactivePropertySlim<bool> CoordinateIsEnabled { get; } = new(true);
+        public ReactivePropertySlim<bool> SnapIsEnabled { get; } = new(true);
+        public ReactivePropertySlim<bool> ShadingIsEnabled { get; } = new(true);
+        public ReactivePropertySlim<bool> LayerIsEnabled { get; } = new(true);
     }
 }
