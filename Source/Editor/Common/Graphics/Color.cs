486e392365ca304c7249535a8ce72fc38c147d82
Gajumaru314159 <igm6323@gmail.com>
2024-10-13 20:29:54 +0900

    ColorとVector2を追加

---
+++ Source/Editor/Common/Graphics/Color.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Graphics
+{
+    public struct Color
+    {
+        public float R { get; set; }
+        public float G { get; set; }
+        public float B { get; set; }
+    }
+}
---
+++ Source/Editor/Common/Math/Vector2.cs
@@ -0,0 +1,81 @@
+﻿namespace Common.Math
+{
+    public struct Vector2
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public static Vector2 Zero => new Vector2(0, 0);
+        public static Vector2 One => new Vector2(1, 1);
+
+        public Vector2(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public static Vector2 operator +(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static Vector2 operator -(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.X - b.X, a.Y - b.Y);
+        }
+
+        public static Vector2 operator *(Vector2 a, float scalar)
+        {
+            return new Vector2(a.X * scalar, a.Y * scalar);
+        }
+
+        public static Vector2 operator /(Vector2 a, float scalar)
+        {
+            return new Vector2(a.X / scalar, a.Y / scalar);
+        }
+
+        public static bool operator ==(Vector2 a, Vector2 b)
+        {
+            return a.X == b.X && a.Y == b.Y;
+        }
+
+        public static bool operator !=(Vector2 a, Vector2 b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Vector2)
+            {
+                return this == (Vector2)obj;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() ^ Y.GetHashCode();
+        }
+
+        public float Length()
+        {
+            return (float)System.Math.Sqrt(X * X + Y * Y);
+        }
+
+        public float SqrLength()
+        {
+            return X * X + Y * Y;
+        }
+
+        public Vector2 Normalize()
+        {
+            float length = Length();
+            if (length > 0)
+            {
+                return this / length;
+            }
+            return this;
+        }
+    }
+}
