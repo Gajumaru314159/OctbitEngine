aeb0eba7e83a9edc476b70bd272204ead648fc43
Gajumaru314159 <igm6323@gmail.com>
2024-12-02 02:54:14 +0900

    ECS実装

--- Source/Framework/Engine/Public/Framework/Engine/ECS.h
+++ Source/Framework/Engine/Public/Framework/Engine/ECS.h
@@ -9,7 +9,79 @@
 #include <Framework/Core/String/StringSplitView.h>
 
 
-#if false
+#if true
+
+namespace ob::engine2 {
+
+	class Archetype {
+	public:
+		template<class... TComponents>
+		static Archetype Create() {
+			Archetype archetype;
+			archetype.m_types = { Type::Get<TComponents>()... };
+			return archetype;
+		}
+	public:
+
+		Archetype() = default;
+
+		Archetype(Span<Type> types) {
+			for (auto& type : types) {
+				m_types.push_back(type);
+			}
+		}
+
+		Archetype(StringView types) {
+			for (auto type : CommaSplitView(types)) {
+				if (auto info = TypeInfo::Find(type)) {
+					m_types.push_back(info->type);
+				}
+			}
+		}
+
+		auto begin() const {
+			return m_types.begin();
+		}
+		auto end() const {
+			return m_types.end();
+		}
+
+		bool operator==(const Archetype& other) const {
+			return m_types == other.m_types;
+		}
+
+	private:
+
+		template<class... TComponents>
+		static s32 Index(bool readonly = true) {
+			static s32 index = GenerateIndex(readonly);
+			return index;
+		}
+
+		static s32 GenerateIndex(bool readonly) {
+			OB_ASSERT(readonly, "読み取り専用のタイミングでアクセスされました。");
+			static s32 index = 0;
+			return index++;
+		}
+	private:
+		FixedVector<Type, 8> m_types;
+	};
+
+}
+
+template<>
+struct std::hash<ob::engine2::Archetype> {
+	size_t operator()(const ob::engine2::Archetype& value)const {
+		size_t hash = 0;
+		for (auto& type : value) {
+			ob::core::Hash::Combine(hash, type);
+		}
+		return hash;
+	}
+};
+
+
+
 namespace ob::engine2 {
 
 
@@ -26,7 +98,11 @@ namespace ob::engine2 {
 			(TypeIndex<T, N, Rest...>::value == -1 ? -1 : 1 + TypeIndex<T, N, Rest...>::value);
 	};
 
-
+	// 型が見つからない場合の特殊化
+	template<typename T, int N>
+	struct TypeIndex<T, N> {
+		static constexpr int value = -1;
+	};
 
 	struct Entity {
 		u32 index;
@@ -35,59 +111,30 @@ namespace ob::engine2 {
 	};
 
 
-	class Archetype {
+
+	class AnyVector 
+		//: Noncopyable 
+	{
 	public:
-		template<class... TComponents>
-		Archetype Create() {
-			return { {Type::Get<TComponents>()...} , Index<TComponents...>() };
-		}
-	public:
-
-		Archetype(StringView types) {
-			for (auto type : CommaSplitView(types)) {
-				if (auto info = TypeInfo::Find(type)) {
-					m_types.push_back(info->type);
-				}
-			}
-		}
-
-		s32 index() const {
-			return m_index;
-		}
-	private:
-
-		template<class... TComponents>
-		static s32 Index(bool readonly = true) {
-			static s32 index = GenerateIndex(readonly);
-			return index;
-		}
-
-		static s32 GenerateIndex(bool readonly) {
-			OB_ASSERT(readonly, "読み取り専用のタイミングでアクセスされました。");
-			static s32 index = 0;
-			return index++;
-		}
-	private:
-		FixedVector<Type, 8> m_types;
-		s32 m_index;
-	};
-
-
-	class AnyVector : Noncopyable {
-	public:
-		AnyVector(Type type) {
-			m_info = TypeInfo::Find(type);
-			OB_ASSERT(m_info, "{}をリフレクション登録してください", type.name());
+		AnyVector(const TypeInfo& info) 
+			: m_info(&info)
+		{
 			m_constructor = m_info->findConstructor();
-			OB_ASSERT(m_info, "{}にデフォルトコンストラクタを追加してください", type.name());
+			OB_ASSERT(m_constructor, "{}にデフォルトコンストラクタを追加してください", info.type.name());
+
+			const auto l1cache = 16 * 1024;
 
 			m_size = 0;
-			m_capacity = 16 * 1024 / m_info->stride();
+			m_capacity = l1cache / m_info->stride();
 			m_blob.resize(m_capacity * m_info->stride());
 		}
+		~AnyVector() {
+			for (s32 i = 0; i < m_size; ++i) {
+				m_info->destroyPlaced(at(i));
+			}
+		}
 		void push_back() {
-			auto ptr = GetOffsetPtr(m_blob.data(), m_info->stride() * m_size);
-			m_info->findConstructor();
+			auto ptr = at(m_size);
 			m_constructor->placedInvoker(ptr, {});
 			m_size++;
 		}
@@ -95,7 +142,16 @@ namespace ob::engine2 {
 			return m_size;
 		}
 		size_t capacity() const {
-			return m_size;
+			return m_capacity;
+		}
+		bool full() const {
+			return m_size == m_capacity;
+		}
+		void* at(size_t index) {
+			return GetOffsetPtr(m_blob.data(), m_info->stride() * index);
+		}
+		const void* at(size_t index) const {
+			return GetOffsetPtr(m_blob.data(), m_info->stride() * index);
 		}
 	private:
 		const TypeInfo* m_info;
@@ -108,60 +164,116 @@ namespace ob::engine2 {
 
 	class ComponentAllocator {
 	public:
-		ComponentAllocator(Type type)
+		ComponentAllocator(const TypeInfo& info)
+			: m_info(info)
 		{
-			m_info = TypeInfo::Find(type);
-			OB_ASSERT(m_info, "{}をリフレクション登録してください",type.name());
-			m_chunks.emplace_back(m_info->stride());
+			m_constructor = m_info.findConstructor();
+			OB_ASSERT(m_constructor, "{}にデフォルトコンストラクタを追加してください", info.type.name());
+			m_span = m_info.stride();
+			m_chunks.emplace_back(m_info);
 		}
 
-		void push_back() {
+		s32 push_back() {
 
 			auto* chunk = &m_chunks.back();
-
+			
 			// 現在のチャンクがいっぱいの場合新しいチャンクを生成
-			if (chunk->size() + 1 == chunk->capacity()) {
-				m_chunks.emplace_back(type);
-				chunk = &m_chunks.emplace_back(m_info->stride());
-				chunk->reserve(m_span);
+			if (chunk->full()) {
+				chunk = &m_chunks.emplace_back(m_info);
 			}
-
+			
 			auto index = size();
-
+			
 			// チャンクに要素を追加
-			chunk->emplace_back();
-
+			chunk->push_back();
+			
 			return index;
+		}
+
+		void init(size_t index) {
+			auto ptr = at(index);
+			m_info.destroyPlaced(ptr);
+			m_constructor->placedInvoker(ptr, {});
 		}
 
 		s32 size() const {
 			return (m_chunks.size() - 1) * m_span + m_chunks.back().size();
 		}
 
+		void* at(size_t index) {
+			return m_chunks.at(index / m_span).at(index % m_span);
+		}
+		const void* at(size_t index) const {
+			return m_chunks.at(index / m_span).at(index % m_span);
+		}
+
+		template<class T>
 		T& at(size_t index) {
-			return m_chunks.at(index / m_span).at(index % m_span);
+			return reinterpret_cast<T*>(at(index));
 		}
+
+		template<class T>
 		const T& at(size_t index) const {
-			return m_chunks.at(index / m_span).at(index % m_span);
+			return reinterpret_cast<const T*>(at(index));
 		}
+
+		const TypeInfo& info() const {
+			return m_info;
+		}
+
 	private:
+		const TypeInfo& m_info;
 		Vector<AnyVector> m_chunks;
+		size_t m_span;
+		const ConstructorInfo* m_constructor;
 	};
 
 
-
+	//! @brief Archetype毎にComponentを管理するクラス
 	class Chunk {
 	public:
-		Chunk(Span<Type> types) {
-			for (auto type : types) {
+		Chunk(Archetype archetype)
+			: m_archetype(archetype)
+		{
+			for (auto& type : archetype) {
 				if (auto info = TypeInfo::Find(type)) {
-					auto& chunk = m_chunks[info->type];
-					chunk.info = info;
-
-				} else {
-					throw Exception(Format(("{}をリフレクション登録してください", type.name())));
+					m_chunks.emplace(info->type, *info);
+				}
+				else {
+					throw Exception(Format("{}をリフレクション登録してください", type.name()));
 				}
 			}
+		}
+
+		s32 create() {
+
+			if (m_free.empty()) {
+				s32 index = 0;
+				for (auto& [type, chunk] : m_chunks) {
+					index = chunk.size();
+					chunk.push_back();
+				}
+				return index;
+			}
+
+			auto index = m_free.back();
+			return index;
+		}
+		void destroy(s32 index) {
+			// TODO 範囲外チェック
+			// TODO 二重開放チェック
+			m_free.push_back(index);
+			for (auto& [type, chunk] : m_chunks) {
+				chunk.init(index);
+			}
+		}
+
+		Any get(Type type, u32 index) {
+			auto itr = m_chunks.find(type);
+			if (itr == m_chunks.end()) return {};
+
+			auto& chunk = itr->second;
+			return Any(chunk.info(), chunk.at(index));
 		}
 
 		template<class T>
@@ -169,20 +281,21 @@ namespace ob::engine2 {
 			return get(Type::Get<T>(), index);
 		}
 
-		Any get(Type type, u32 index) {
-			auto itr = m_chunks.find(type);
-			if (itr != m_chunks.end()) return {};
+		template<class... TComponents>
+		void visit(Func<void(TComponents...)> func) {
 
-			auto& chunk = itr->second;
+			// TODO 型チェック
+			auto types = { Type::Get<TComponents>()... };
 
-			s32 stride = chunk.info->stride;
-			
-			Any component(*chunk.info,GetOffsetPtr(chunk.blob.data(), stride * index));
+			ComponentAllocator* chunks[] = { &m_chunks[Type::Get<TComponents>()]... };
 
-			return component;
-		}
-
-		s32 create() {
+			size_t size = 0;
+			for (s32 i = 0; i < size; ++i) {
+				Tuple<TComponents&...> components{
+					(*reinterpret_cast<TComponents*>(m_chunks[Type::Get<TComponents>()].at(i))) ...
+				};
+				std::apply(func, components);
+			}
 
 		}
 
@@ -194,55 +307,104 @@ namespace ob::engine2 {
 	private:
 		Archetype m_archetype;
 
-		HashMap<Type, ChunkData> m_chunks;
+		HashMap<Type, ComponentAllocator> m_chunks;
+		Vector<s32> m_free;
+
 	};
 
-	// 型が見つからない場合の特殊化
-	template<typename T, int N>
-	struct TypeIndex<T, N> {
-		static constexpr int value = -1;
-	};
 
 	class ECS {
 	public:
 
-		template<class... TComponents>
-		Entity create() {
-			return create(Archetype::Create<TComponents...>());
-		}
-
-		Entity create(StringView archetype) {
-			return create(archetype);
-		}
+		//! @brief Archetypeを指定して新しいEntityを生成
 		Entity create(Archetype archetype) {
-			auto [itr, added] = m_chunks.try_emplace(archetype);
+			auto [itr, added] = m_chunks.try_emplace(archetype, archetype);
+			if (added) {
+				auto index = m_indices.size();
+				m_indices[archetype] = index;
+				m_archetypes[index] = archetype;
+			}
+
 			Chunk& chunk = itr->second;
 
 			Entity entity;
 			entity.index = chunk.create();
-			entity.archetype = archetype.index();
+			entity.archetype = m_indices[archetype];
 			entity.version = 0;
+			return entity;
+		}
+		template<class... TComponents>
+		Entity create() {
+			return create(Archetype::Create<TComponents...>());
+		}
+		Entity create(StringView archetype) {
+			return create(Archetype(archetype));
 		}
 
+		//! @brief Archetypeを変更して新しいEntityを生成
+		//! @details 元のEntityは破棄される
+		Entity map(Archetype archetype, Entity from) {
+			return {}; // TODO
+		}
+		template<class... TComponents>
+		Entity map(Entity from) {
+			return map(Archetype::Create<TComponents...>(), from);
+		}
+		Entity map(StringView archetype, Entity from) {
+			return map(Archetype(archetype), from);
+		}
+
+		//! @brief 同じArchetypeのEntityを複製 
+		Entity duplicate(Entity from) {
+			return {}; // TODO
+		}
+		//! @brief Archetypeを指定してEntityを複製
+		//! @details 不足するComponentはデフォルトコンストラクタで生成され、余分なComponentは破棄される。
+		template<class... TComponents>
+		Entity duplicateWith(Entity from) {
+			// TODO
+			return {};// create(Archetype::Create<TComponents...>());
+		}
+
+		//! @brief EntityとComponentを破棄する 
 		void destroy(Entity entity) {
-			//m_chunks[entity.archetype]->destroy(entity.index);
+			auto itr = m_archetypes.find(entity.archetype);
+			if (itr == m_archetypes.end()) return;
+			auto& archetype = itr->second;
+
+			auto itr2 = m_chunks.find(archetype);
+			if (itr2 == m_chunks.end()) return;
+			auto& chunk = itr2->second;
+
+			chunk.destroy(entity.index);
 		}
 
+		//! @brief タイプを指定してEntityにバインドされているComponentを取得する
+		//! @note  型チェックが発生するため多用は避ける
 		Any get(Type type, Entity entity) {
-			return m_chunks[entity.archetype]->get(type,entity.index);
-		}
+			auto itr = m_archetypes.find(entity.archetype);
+			if (itr == m_archetypes.end()) return {};
+			auto& archetype = itr->second;
 
+			auto itr2 = m_chunks.find(archetype);
+			if (itr2 == m_chunks.end()) return {};
+			auto& chunk = itr2->second;
+
+			return chunk.get(type, entity.index);
+		}
+		Any get(StringView type, Entity entity) {
+			return get(Type(type), entity);
+		}
 		template<class TComponent>
 		TComponent& get(Entity entity) {
-			m_chunks[entity.archetype]->get<TComponent>(entity.index);
+			return get(Type::Get<TComponent>(), entity).as<TComponent>();
 		}
 
+
+
+
 		template<class TSystem, class... Args>
-		auto update() {
-			//TSystem::Update();
-		}
-		template<class TSystem, class... Args>
-		auto update() {
+		void update() {
 			//TSystem::Update();
 		}
 
@@ -256,10 +418,12 @@ namespace ob::engine2 {
 			mask.flip();
 
 			// 全てのComponentを持つチャンクを抽出
+			// NOTE 抽出は毎フレームしなくてよくはないか？
 			for (auto& type : types) {
 				mask &= m_availables[type]
 			}
 
+			// 型チェックはChunk * Archetype.size()回発生する
 			for (s32 i = 0; i < mask.size(); ++i) {
 				if (mask[i]) {
 					m_chunks.at(i);
@@ -270,6 +434,7 @@ namespace ob::engine2 {
 
 		void merge(ECS& ecs) {
 			// 他のECSのComponentを自分のECSに移す
+			// Entityのインデックスを再割り当てしないといけない？
 		}
 	private:
 
@@ -277,7 +442,9 @@ namespace ob::engine2 {
 
 		HashMap<Type, ArchetypeMask> m_availables;
 
-		HashMap<Archetype,Chunk> m_chunks;
+		HashMap<Archetype, Chunk> m_chunks;
+		HashMap<Archetype, s32> m_indices;
+		HashMap<s32, Archetype> m_archetypes;
 	};
 
 
@@ -330,8 +497,8 @@ namespace ob::engine2 {
 
 	class RigidbodySystem {
 	public:
-		static void Update(TransformComponent& transform,RigidbodyComponent& rigidbody) {
-			transform.local.position += rigidbody.velocity ;
+		static void Update(TransformComponent& transform, RigidbodyComponent& rigidbody) {
+			transform.local.position += rigidbody.velocity;
 		}
 	};
 
@@ -358,14 +525,21 @@ namespace ob::engine2 {
 		ECS ecs;
 
 		auto entity0 = ecs.create<TransformComponent>();
-		auto entity1 = ecs.create<TransformComponent,RigidbodyComponent>();
+		auto entity1 = ecs.create<TransformComponent, RigidbodyComponent>();
+		auto entity2 = ecs.create("TransformComponent,RigidbodyComponent");
 
-		auto entity5 = ecs.create("TransformComponent,RigidbodyComponent");
-		auto comp = ecs.get(Type("Transform"), entity5);
-
+		ecs.get<TransformComponent>(entity0);
+		ecs.get("TransformComponent", entity0).as<TransformComponent>();
 
 		ecs.destroy(entity1);
-		ecs.get<TransformComponent>(entity0);
+
+		// 元EntityからAdd/RemoveしてComponentを再生成
+		auto entity3 = ecs.map<TransformComponent>(entity0);
+
+		// 元EntityからAdd/RemoveしてComponentを複製
+		auto entity4 = ecs.duplicate(entity0);
+		auto entity5 = ecs.duplicateWith<TransformComponent>(entity0);
+
 
 
 		ecs.update<RigidbodySystem>();
---
+++ Source/Test/Framework/Engine/Source/ECS.cpp
@@ -0,0 +1,46 @@
+﻿//***********************************************************
+//! @file
+//! @brief		Box のテスト
+//! @author		Gajumaru
+//***********************************************************
+#include <Framework/Core/Reflection/TypeInfoManager.h>
+#include <Framework/Core/Reflection/TypeBuilder.h>
+#include <Framework/Engine/ECS.h>
+
+using namespace ob;
+using namespace ob::engine2;
+
+
+OB_DEFINE_CLASS_INFO(ob::engine2::TransformComponent) {
+	constructor();
+	field("local", &T::local);
+	field("world", &T::world);
+}
+OB_DEFINE_CLASS_INFO(ob::engine2::RigidbodyComponent) {
+	constructor();
+	field("velocity", &T::velocity);
+}
+
+
+
+TEST(ECS, Construct) {
+	TypeInfoManager manager;
+
+	ECS ecs;
+
+	auto entity0 = ecs.create<TransformComponent>();
+	auto entity1 = ecs.create<TransformComponent, RigidbodyComponent>();
+	auto entity2 = ecs.create("ob::engine2::TransformComponent,ob::engine2::RigidbodyComponent");
+
+	auto& trans = ecs.get<TransformComponent>(entity0);
+	trans.local.position.set(1, 2, 3);
+	ecs.destroy(entity0);
+	entity0 = ecs.create<TransformComponent>();
+	auto& trans2 = ecs.get<TransformComponent>(entity0);
+
+
+	auto& trans3 = ecs.get("ob::engine2::TransformComponent", entity0).as<TransformComponent>();
+
+	ecs.destroy(entity1);
+
+}
