ca637cd7493f7c27b2801685b10663af224801bb
Gajumaru314159 <igm6323@gmail.com>
2024-12-07 20:31:47 +0900

    実行ディレクトリの設定を削除

--- Source/Test/Framework/Core/Source/main.cpp
+++ Source/Test/Framework/Core/Source/main.cpp
@@ -6,9 +6,6 @@
 #include <filesystem>
 
 int main(int argc, char** argv) {
-    
-    std::filesystem::current_path("D:/test");
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
