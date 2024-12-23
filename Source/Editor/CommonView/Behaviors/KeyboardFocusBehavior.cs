b0fe5c0e1577f4ea422e5f406d81210d2f5dc1db
Gajumaru314159 <igm6323@gmail.com>
2024-12-24 03:54:24 +0900

    KeyGestureで検索ボックスにフォーカスできるようにする

---
+++ Source/Editor/CommonView/Behaviors/KeyboardFocusBehavior.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CommonView.Behaviors
+{
+    public class KeyboardFocusBehavior : Behavior<UIElement>
+    {
+        public static readonly DependencyProperty TargetProperty =
+            DependencyProperty.Register(nameof(Target), typeof(UIElement), typeof(KeyboardFocusBehavior));
+
+        public UIElement Target
+        {
+            get { return (UIElement)GetValue(TargetProperty); }
+            set { SetValue(TargetProperty, value); }
+        }
+
+        public static readonly DependencyProperty InputGestureProperty =
+            DependencyProperty.Register(nameof(InputGesture), typeof(KeyGesture), typeof(KeyboardFocusBehavior));
+
+        public KeyGesture InputGesture
+        {
+            get { return (KeyGesture)GetValue(InputGestureProperty); }
+            set { SetValue(InputGestureProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.KeyDown += OnKeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.KeyDown -= OnKeyDown;
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (InputGesture != null && InputGesture.Matches(null, e) && Target != null)
+            {
+                Target.Focus();
+                Keyboard.Focus(Target);
+                e.Handled = true;
+            }
+        }
+    }
+}
