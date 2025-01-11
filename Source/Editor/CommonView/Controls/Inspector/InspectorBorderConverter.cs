85ae40fb187feeab19ab66fbef4923ef9a7929fc
Gajumaru314159 <igm6323@gmail.com>
2025-01-12 00:56:09 +0900

    FramewokElementではないRenderTransformに直接バインディングできないのでMarginで表示位置を調整するように変更

---
+++ Source/Editor/CommonView/Controls/Inspector/InspectorBorderConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CommonView
+{
+    public class InspectorBorderConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not double doubleValue) return new Thickness(0, 0, 0, 0);
+            return new Thickness(doubleValue-7, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
