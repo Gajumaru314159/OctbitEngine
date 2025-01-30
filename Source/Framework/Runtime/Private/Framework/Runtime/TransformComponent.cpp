1bc873a604c61c9ec6609487e1013ac9f86c854e
Gajumaru314159 <igm6323@gmail.com>
2025-01-31 01:22:42 +0900

    Runtimeライブラリの追加検証

---
+++ Source/Framework/Runtime/Private/Framework/Runtime/TransformComponent.cpp
@@ -0,0 +1,21 @@
+﻿//***********************************************************
+//! @file
+//! @brief		コンポーネント
+//! @author		Gajumaru
+//***********************************************************
+#include <Framework/Engine/Component/TransformComponent.h>
+#include <fstream>
+
+#define DECLSPEC extern "C" __declspec(dllexport)
+
+using namespace ob::core;
+
+DECLSPEC void __stdcall Camera_SetPosition(ob::engine::TransformComponent* transform, f32 x, f32 y, f32 z) {
+	transform->setLocalPosition({ x,y,z });
+}
+
+
+DECLSPEC void __stdcall ExportText() {
+	std::ofstream s("D:/sample.txt");
+	s << "Hello World!";
+}
