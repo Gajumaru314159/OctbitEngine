92f25311796ed05917a33a967f20f1e365cd6866
Gajumaru314159 <igm6323@gmail.com>
2025-03-20 03:09:36 +0900

    ErrorCodeを追加

---
+++ Source/Framework/Core/Private/Framework/Core/Misc/ErrorCode.cpp
@@ -0,0 +1,40 @@
+﻿//***********************************************************
+//! @file
+//! @brief Blob
+//! @author Gajumaru
+//***********************************************************
+#include <Framework/Core/Misc/ErrorCode.h>
+#include <Framework/Core/Platform/WindowsHeaders.h>
+#include <Framework/Core/String/StringEncoder.h>
+
+namespace ob::core {
+
+    String ErrorCode::message() const {
+#ifdef OS_WINDOWS
+        WCHAR buffer[256];
+        DWORD dwFlags = FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS;
+        auto result = ::FormatMessageW(
+            dwFlags,
+            nullptr,
+            m_value,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            buffer,
+            (DWORD)std::size(buffer),
+            nullptr
+        );
+
+        if (0 == result) {
+            String message;
+            StringEncoder::Encode(buffer, message);
+            return "不明なエラー";
+        }
+        String message;
+        StringEncoder::Encode(buffer, message);
+        if (2 <= message.size())message.resize(message.size() - 2);
+        return message;
+#else
+        return "";
+#endif
+    }
+
+} 
---
+++ Source/Framework/Core/Public/Framework/Core/Misc/ErrorCode.h
@@ -0,0 +1,41 @@
+﻿//***********************************************************
+//! @file
+//! @brief		バイナリデータ(Binary Large Object)
+//! @author		Gajumaru
+//***********************************************************
+#pragma once
+#include <Framework/Core/CoreTypes.h>
+#include <Framework/Core/String/String.h>
+
+namespace ob::core {
+
+    //! @brief エラーコード
+    class ErrorCode {
+    public:
+        ErrorCode() : m_value(0) {}
+        ErrorCode(u32 value) : m_value(value) {}
+
+        u32 value() const { return m_value; }
+        String message() const;
+
+    private:
+        u32 m_value;
+    };
+
+}
+
+//===============================================================
+// フォーマット
+//===============================================================
+//! @cond
+template <> struct std::formatter<ob::core::ErrorCode, ob::core::Char> {
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) -> decltype(ctx.begin()) {
+        return ctx.end();
+    }
+
+    template<typename FormatContext>
+    auto format(ob::core::ErrorCode value, FormatContext& ctx) const {
+        return format_to(ctx.out(), "0x{:08X} {}", value.value(), value.message().c_str());
+    }
+};
