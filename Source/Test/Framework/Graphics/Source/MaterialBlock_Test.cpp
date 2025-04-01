f96c94c57828cfcc40b19e993ad110995234e0b5
Gajumaru314159 <igm6323@gmail.com>
2025-04-02 02:00:25 +0900

    マルチバインドテスト

--- Source/Test/Framework/Graphics/Source/MaterialBlock_Test.cpp
+++ Source/Test/Framework/Graphics/Source/MaterialBlock_Test.cpp
@@ -516,3 +516,283 @@ PsOut PS_Main(PsIn i){
 	}
 
 }
+
+
+
+TEST(MaterialBlock, MultiBindless) {
+#pragma region
+	using namespace ob;
+	using namespace ob::rhi;
+	using namespace ob::graphics;
+	using namespace ob::platform;
+	ob::core::Logger log;
+
+	System::Setup();
+
+	ServiceInjector injector;
+	ServiceContainer container;
+	{
+		rhi::dx12::RegisterDirectX12RHIService(injector);
+		graphics::RegisterGraphicsService(injector);
+		{
+			rhi::RHIConfig config;
+			// config.enableDebugLayer = true;
+			config.enableBindless = true;
+			config.enablePIX = true;
+			injector.bind(config);
+		}
+
+		struct Dependency {
+			Dependency(ob::graphics::Graphics&, SystemResource&) {}
+		};
+		injector.bind<Dependency>();
+
+		injector.create<Dependency>(container);
+	}
+
+
+	// ウィンドウ生成
+	platform::WindowDesc windowDesc;
+	windowDesc.title = "MaterialBlock MultiBindless";
+	platform::Window window(windowDesc);
+	window.show();
+
+	Ref<Display> display = [&] {
+		DisplayDesc desc;
+		desc.name = "MainDisplay";
+		desc.window = window;
+		return Display::Create(desc);
+		}();
+
+#pragma endregion
+
+	struct Vertex {
+		Vec2 pos;
+		Vec2 uv;
+	};
+
+	Ref<Buffer> vertexBuffer;
+	Ref<Buffer> indexBuffer;
+	{
+		Vertex vertices[] = {
+			{Vec2(-1,-1),Vec2(0,1)},
+			{Vec2(-1,1),Vec2(0,0)},
+			{Vec2(1,1),Vec2(1,0)},
+			{Vec2(1,-1),Vec2(1,1)},
+		};
+		u16 indices[] = { 0,1,2,0,2,3 };
+
+		vertexBuffer = Buffer::Create(BufferDesc::Vertex<Vertex>(4));
+		indexBuffer = Buffer::Create(BufferDesc::Index<u16>(6));
+
+		vertexBuffer->updateDirect(sizeof(vertices), vertices);
+		indexBuffer->updateDirect(sizeof(indices), indices);
+	}
+
+	// 事前セットアップここまで
+	Ref<Shader> vs;
+	Ref<Shader> ps;
+	{
+		String code = R"(
+
+struct TextureHandle {
+	uint index;
+	uint type;
+	uint padding[2];
+};
+
+struct SamplerHandle {
+	uint index;
+	uint type;
+	uint padding[2];
+};
+
+struct BufferHandle {
+	uint index;
+	uint type;
+	uint padding[2];
+};
+
+struct Param {
+	TextureHandle MainTexture;
+	SamplerHandle MainSampler;
+	BufferHandle Buffer;
+	float4 Color;
+	float Progress;
+	float Speed;
+	float Time;
+	float Width;
+	float Height;
+};
+
+struct Param2 {
+	float Scale;
+};
+
+ConstantBuffer<BufferHandle> ParamHandle : register(b0);
+ConstantBuffer<BufferHandle> Param2Handle : register(b1);
+
+											
+// IN / OUT												
+struct VsIn {												
+	float2 pos		:POSITION;									
+	float2 uv	    :TEXCOORD0;										
+};															
+struct PsIn {												
+	float4 pos		:SV_POSITION;								
+	float2 uv	    :TEXCOORD0;									
+};															
+// エントリ													
+struct PsOut {												
+	float4 color	:SV_TARGET0;								
+};															
+PsIn VS_Main(VsIn i) {
+	PsIn o;													
+	o.pos = float4(i.pos,0,1);			
+
+	Param2 param2 = ByteAddressBuffer(ResourceDescriptorHeap[Param2Handle.index]).Load<Param2>(0);		        
+	o.uv  = i.uv * param2.Scale;
+	return o;
+}
+PsOut PS_Main(PsIn i){										
+	PsOut o;					
+	Param param = ByteAddressBuffer(ResourceDescriptorHeap[ParamHandle.index]).Load<Param>(0);
+
+	Texture2D g_mainTex = ResourceDescriptorHeap[param.MainTexture.index];							
+	SamplerState g_mainSampler = SamplerDescriptorHeap[param.MainSampler.index];							
+	o.color = g_mainTex.Sample(g_mainSampler,i.uv * float2(param.Width/16,param.Height/16) + float2(param.Time*param.Speed,param.Time*param.Speed))*param.Color;	
+	o.color.xyz *= 1 - step(i.uv.x,param.Progress) * step(i.uv.y,0.1);
+	return o;											        
+}																
+
+)";
+		vs = Shader::CompileVS(code);
+		ps = Shader::CompilePS(code);
+		OB_ASSERT_EXPR(vs && ps);
+	}
+
+	Ref<RootSignature> signature;
+	{
+		RootSignatureDesc desc({ RootParameter::Constants(16,0,0),RootParameter::Constants(16,1,0) });
+		desc.name = "MaterialBlock";
+		desc.flags.set(RootSignatureFlag::EnableBindless, true);
+		signature = RootSignature::Create(desc);
+		OB_ASSERT_EXPR(signature);
+	}
+
+	Ref<PipelineState> pipeline;
+	{
+		PipelineStateDesc desc;
+		desc.name = "MaterialBlock";
+		desc.colors = { TextureFormat::RGBA8 };
+
+		desc.rootSignature = signature;
+		desc.vs = vs;
+		desc.ps = ps;
+		desc.vertexLayout.attributes = {
+			VertexAttribute(Semantic::Position,offsetof(Vertex,pos),ElementType::Float,2),
+			VertexAttribute(Semantic::TexCoord,offsetof(Vertex,uv),ElementType::Float,2),
+		};
+
+		pipeline = PipelineState::Create(desc);
+		OB_ASSERT_EXPR(pipeline);
+	}
+
+
+	MaterialBlockDesc desc;
+	desc.name = "Param";
+	desc.textures = { "Texture" };
+	desc.buffers = { "Buffer" };
+	desc.vectors = { "Color" };
+	desc.scalars = { "Progress","Speed", "Time", "Width", "Height" };
+
+	MaterialBlock block(desc);
+	block.setScalar("Speed", 3.f);
+	block.setScalar("Width", window.getSize().x);
+	block.setScalar("Height", window.getSize().y);
+	block.setTexture("Texture", Texture::Check(), Sampler::Default());
+	block.setVector("Color", Color::Cyan);
+
+
+
+	MaterialBlockDesc desc2;
+	desc2.name = "Param2";
+	desc2.scalars = { "Scale" };
+
+	MaterialBlock block2(desc2);
+	block2.setScalar("Scale", 3.f);
+
+	auto start = DateTime::Now();
+
+	Ref<CommandList> commandList;
+	{
+		CommandListDesc desc;
+		desc.name = "Main";
+		desc.type = CommandListType::Graphic;
+		commandList = CommandList::Create(desc);
+	}
+
+
+	Ref<RenderTexture> renderTexture;
+	{
+		RenderTextureDesc desc;
+		desc.name = "RenderTexture";
+		desc.size = display->getDesc().size;
+		desc.format = TextureFormat::RGBA8;
+		desc.clear.color = Color::Black;
+		desc.display = display;
+		renderTexture = RenderTexture::Create(desc);
+	}
+
+	f32 endTime = 5;
+
+	while (true) {
+
+		if (System::Update() == false) break;
+
+		auto time = TimeSpan(start, DateTime::Now()).totalSecondsF();
+
+		if (endTime < time)break;
+
+		block.setScalar("Time", time);
+		block.setScalar("Progress", time / endTime);
+
+		commandList->begin();
+
+		Viewport viewport;
+		viewport.right = renderTexture->width();
+		viewport.bottom = renderTexture->height();
+		commandList->setViewport(&viewport, 1);
+
+		commandList->setRenderTarget(renderTexture);
+		commandList->clearColors();
+
+		commandList->setPipelineState(pipeline);
+		block.record(commandList, 0);
+		block2.record(commandList, 1);
+
+		commandList->setVertexBuffer(vertexBuffer);
+		commandList->setIndexBuffer(indexBuffer);
+
+		DrawIndexedParam param;
+		param.indexCount = 6;
+		param.startIndex = 0;
+		param.startVertex = 0;
+		commandList->drawIndexed(param);
+
+		commandList->applyDisplay(display, renderTexture);
+
+		commandList->end();
+		commandList->flush();
+
+		if (auto graphics = container.get<Graphics>()) {
+			graphics->update();
+		}
+
+		RHI::Get()->update();
+		display->update();
+
+		Thread::Sleep(33);
+	}
+
+}
