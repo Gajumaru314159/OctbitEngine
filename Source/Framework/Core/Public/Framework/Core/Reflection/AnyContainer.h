f206b8397461c43bd2be8a686182678e61d0f0a9
Gajumaru314159 <igm6323@gmail.com>
2025-05-17 04:20:12 +0900

    AnyContainerを実装

---
+++ Source/Framework/Core/Public/Framework/Core/Reflection/AnyContainer.h
@@ -0,0 +1,66 @@
+﻿//***********************************************************
+//! @file
+//! @brief		
+//! @author		Gajumaru
+//***********************************************************
+#pragma once
+#include <Framework/Core/Reflection/Type.h>
+#include <Framework/Core/Utility/Noncopyable.h>
+#include <Framework/Core/Utility/Nonmovable.h>
+
+namespace ob::core {
+
+	//! @brief 型ごとにインスタンスを格納・取得できるコンテナクラスです。型Tごとのインスタンスの参照取得、存在判定などの機能を提供します。コピー・ムーブは不可です。
+	class AnyContainer : Nonmovable, Noncopyable {
+	private:
+		struct HolderBase { virtual ~HolderBase() = default; };
+		template<class T> struct Holder : HolderBase { T instance; };
+	public:
+
+		AnyContainer() = default;
+
+		//! @brief 指定した型Tのインスタンスへの参照を取得します。
+		//! @tparam T 取得するインスタンスの型。
+		//! @return 型Tのインスタンスへの参照。
+		template<class T>
+		auto get() -> std::enable_if_t<std::is_default_constructible<T>::value, T&> {
+			constexpr Type type = Type::Get<T>();
+			auto& holder = m_container[type];
+			if (!holder) {
+				holder = std::make_unique<Holder<T>>();
+			}
+			return reinterpret_cast<Holder<T>*>(holder.get())->instance;
+		}
+
+		//! @brief 指定した型のインスタンスへの定数参照を取得します。
+		//! @tparam T 取得するインスタンスの型。
+		//! @return 型Tのインスタンスへのconst参照。指定した型がコンテナに存在しない場合はアサートに失敗します。
+		template<class T>
+		auto get() const -> std::enable_if_t<std::is_default_constructible<T>::value, const T&> {
+			constexpr Type type = Type::Get<T>();
+			OB_ASSERT(m_container.contains(type), "{}はコンテナに含まれていません", type.name());
+			auto& holder = m_container.at(type);
+			return reinterpret_cast<const Holder<T>*>(holder.get())->instance;
+		}
+
+		//! @brief 指定した型の要素がコンテナに含まれているかどうかを判定します。
+		//! @tparam T 検索する要素の型。
+		//! @return 指定した型の要素がコンテナに含まれていれば true、そうでなければ false を返します。
+		template<class T>
+		bool contains() const {
+			constexpr Type type = Type::Get<T>();
+			return m_container.contains(type);
+		}
+
+		//! @brief 指定された型がコンテナに含まれているかどうかを判定します。
+		//! @param type 検索対象となる型の参照。
+		//! @return 型がコンテナに含まれていれば true、そうでなければ false を返します。
+		bool contains(const Type& type) const {
+			return m_container.contains(type);
+		}
+
+	private:
+		HashMap<Type, UPtr<HolderBase>> m_container;
+	};
+
+}
---
+++ Source/Test/Framework/Core/Source/Reflection/AnyContainer_Test.cpp
@@ -0,0 +1,71 @@
+﻿//***********************************************************
+//! @file
+//! @brief		Box のテスト
+//! @author		Gajumaru
+//***********************************************************
+#include <Framework/Core/Reflection/AnyContainer.h>
+
+using namespace ob;
+
+TEST(AnyContainer, Get) {
+
+	struct Hoge {
+		int a;
+		float b;
+	};
+
+
+	struct Hoge2 {
+		int& a;
+		float& b;
+	};
+
+	AnyContainer container;
+
+	container.get<int>() = 10;
+	container.get<float>() = 20.0f;
+	container.get<Hoge>() = Hoge{ 1, 2.0f };
+
+	EXPECT_EQ(container.get<int>(), 10);
+	EXPECT_EQ(container.get<float>(), 20.0f);
+	EXPECT_EQ(container.get<Hoge>().a, 1);
+	EXPECT_EQ(container.get<Hoge>().b, 2.0f);
+
+	container.get<int>() = 11;
+	container.get<float>() = 22.0f;
+	container.get<Hoge>().a = 11;
+	container.get<Hoge>().b = 22.0f;
+	
+	EXPECT_EQ(container.get<int>(), 11);
+	EXPECT_EQ(container.get<float>(), 22.0f);
+	EXPECT_EQ(container.get<Hoge>().a, 11);
+	EXPECT_EQ(container.get<Hoge>().b, 22.0f);
+	
+	// デフォルトコンストラクタのないクラスは追加できません
+	// container.get<Hoge2>();
+
+}
+
+TEST(AnyContainer, Contains) {
+	struct Hoge {
+		int a;
+		float b;
+	};
+	AnyContainer container;
+	EXPECT_FALSE(container.contains<int>());
+	EXPECT_FALSE(container.contains<float>());
+	EXPECT_FALSE(container.contains<Hoge>());
+	container.get<int>() = 10;
+	container.get<float>() = 20.0f;
+	container.get<Hoge>() = Hoge{ 1, 2.0f };
+	EXPECT_TRUE(container.contains<int>());
+	EXPECT_TRUE(container.contains<float>());
+	EXPECT_TRUE(container.contains<Hoge>());
+	container.get<int>() = 11;
+	container.get<float>() = 22.0f;
+	container.get<Hoge>().a = 11;
+	container.get<Hoge>().b = 22.0f;
+	EXPECT_TRUE(container.contains<int>());
+	EXPECT_TRUE(container.contains<float>());
+	EXPECT_TRUE(container.contains<Hoge>());
+}
