d24058c3df04a718597ecdf469ca833111ea04eb
Gajumaru314159 <igm6323@gmail.com>
2025-06-17 02:35:12 +0900

    ControlTestをTestディレクトリに移動

---
+++ Source/Editor/OctbitEditor/Test/DockTest.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OctbitEditor
+{
+    /// <summary>
+    /// Interaction logic for DockTest.xaml
+    /// </summary>
+    public partial class DockTest : UserControl
+    {
+        public DockTest()
+        {
+            InitializeComponent();
+        }
+    }
+}
