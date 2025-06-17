6bebd51fbea4ea71af9a1199f7cfa7bf9339e0e9
Gajumaru314159 <igm6323@gmail.com>
2025-06-17 16:19:53 +0900

    独自のDockingの仕組みを追加

---
+++ Source/Editor/CommonView/Controls/Docking/Docking.cs
@@ -0,0 +1,124 @@
+﻿using Reactive.Bindings;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CommonView.Controls
+{
+    // ViewModel
+    public class DockingBase
+    {
+        protected DockingBase()
+        {
+        }
+    }
+
+    public class DockingLayoutGroup : DockingBase
+    {
+        public ObservableCollection<DockingBase> Items { get; } = new();
+
+        public ReactivePropertySlim<Orientation> Orientation { get; } = new(System.Windows.Controls.Orientation.Horizontal);
+
+        public DockingLayoutGroup()
+        {
+        }
+
+        public void Add(DockingBase item)
+        {
+            Items.Add(item);
+        }
+    }
+
+    public class DockingTabGroup : DockingBase
+    {
+        public ObservableCollection<object> Items { get; } = new();
+
+        public DockingTabGroup()
+        {
+        }
+
+        public void Add(object item)
+        {
+            Items.Add(item);
+        }
+    }
+    public class DockingTab
+    {
+        protected DockingTab(string title)
+        {
+            Title = title;
+        }
+        public string Title { get; private init; } = "Title";
+    }
+
+
+    // View
+    public class DockingLayout : Control
+    {
+        static DockingLayout()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(DockingLayout),
+                new FrameworkPropertyMetadata(typeof(DockingLayout)));
+        }
+
+        public static readonly DependencyProperty ItemProperty = DependencyProperty.Register(
+                nameof(Item),
+                typeof(DockingLayoutGroup),
+                typeof(DockingLayout),
+                new PropertyMetadata());
+
+        public static readonly DependencyProperty ItemTemplateProperty = DependencyProperty.Register(
+                nameof(ItemTemplate),
+                typeof(DataTemplate),
+                typeof(DockingLayout),
+                new PropertyMetadata());
+
+        public static readonly DependencyProperty ContentTemplateProperty = DependencyProperty.Register(
+                nameof(ContentTemplate),
+                typeof(DataTemplate),
+                typeof(DockingLayout),
+                new PropertyMetadata());
+
+
+        public static readonly DependencyProperty ItemTemplateSelectorProperty = DependencyProperty.Register(
+                nameof(ItemTemplateSelector),
+                typeof(DataTemplateSelector),
+                typeof(DockingLayout),
+                new PropertyMetadata());
+
+        public static readonly DependencyProperty ContentTemplateSelectorProperty = DependencyProperty.Register(
+                nameof(ContentTemplateSelector),
+                typeof(DataTemplateSelector),
+                typeof(DockingLayout),
+                new PropertyMetadata());
+        public DockingLayoutGroup Item
+        {
+            get => (DockingLayoutGroup)GetValue(ItemProperty);
+            set => SetValue(ItemProperty, value);
+        }
+        public DataTemplate ItemTemplate
+        {
+            get => (DataTemplate)GetValue(ItemTemplateProperty);
+            set => SetValue(ItemTemplateProperty, value);
+        }
+        public DataTemplate ContentTemplate
+        {
+            get => (DataTemplate)GetValue(ContentTemplateProperty);
+            set => SetValue(ContentTemplateProperty, value);
+        }
+        public DataTemplateSelector ItemTemplateSelector
+        {
+            get => (DataTemplateSelector)GetValue(ItemTemplateSelectorProperty);
+            set => SetValue(ItemTemplateSelectorProperty, value);
+        }
+        public DataTemplateSelector ContentTemplateSelector
+        {
+            get => (DataTemplateSelector)GetValue(ContentTemplateSelectorProperty);
+            set => SetValue(ContentTemplateSelectorProperty, value);
+        }
+
+    }
+
+
+}
---
+++ Source/Editor/CommonView/Utilities/GenericDataTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace CommonView.Controls
+{
+    [ContentProperty(nameof(Templates))]
+    public class GenericDataTemplateSelector : DataTemplateSelector
+    {
+        public Collection<DataTemplate> Templates { get; }
+            = new Collection<DataTemplate>();
+
+        public override DataTemplate? SelectTemplate(object item, DependencyObject container)
+        {
+            if (item == null) return null;
+
+            var itemType = item.GetType();
+            foreach (var template in Templates)
+            {
+                if (template.DataType is Type dataType && dataType.IsAssignableFrom(itemType))
+                {
+                    return template;
+                }
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
