b9190497c183dc213b6308327b06f9aa00381274
Gajumaru314159 <34750484+Gajumaru314159@users.noreply.github.com>
2025-07-19 10:11:58 +0000

    Deploying to gh-pages from @ Gajumaru314159/OctbitEngine@3e3a2aec9cb3fd2ea8c852b933f0e6975410c04a 🚀

--- d0/d2e/classob_1_1rhi_1_1_descriptor_table.js
+++ d0/d2e/classob_1_1rhi_1_1_descriptor_table.js
@@ -1,5 +1,4 @@
 var classob_1_1rhi_1_1_descriptor_table =
 [
-    [ "getBindlessHandle", "d0/d2e/classob_1_1rhi_1_1_descriptor_table.html#aca92130ec1b48fa445399e48ae6699f2", null ],
     [ "setResource", "d0/d2e/classob_1_1rhi_1_1_descriptor_table.html#af8e8c71284c076c47db55e9ac99cfeee", null ]
 ];
