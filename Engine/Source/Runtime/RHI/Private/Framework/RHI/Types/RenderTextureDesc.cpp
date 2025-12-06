ca042e5fa580283fd5691efd3c84562ebb9f497f
Gajumaru <igm6323@gmail.com>
2025-12-06 23:39:08 +0900

    追加漏れ

---
+++ Engine/Source/Runtime/RHI/Private/Framework/RHI/Types/RenderTextureDesc.cpp
@@ -0,0 +1,35 @@
+﻿//***********************************************************
+//! @file
+//! @author		Gajumaru
+//***********************************************************
+#include <magic_enum.hpp>
+#include <Framework/RHI/Types/RenderTextureDesc.h>
+#include <Framework/RHI/RenderTexture.h>
+
+namespace ob::rhi {
+
+    bool RenderTextureDesc::isValid() const {
+
+        // サイズ
+        if (!(0 < size.width && 0 < size.height && 1 == size.depth)) {
+            LOG_ERROR("TextureDescのサイズが不正です。[name={},size={}]", name, size);
+            return false;
+        }
+
+        // ステンシルを含んでいるとそのままShaderResourceとして使用できない
+        if (TextureFormatUtility::HasStencil(format)) {
+            // TODO D24S8の扱いを考える(Plane指定？)
+            LOG_ERROR("ステンシルを含むテクスチャはShaderResourceとして使用できません [name={}]", name);
+            return false;
+        }
+
+        // フォーマット
+        if (!RenderTexture::Supports(format)) {
+            LOG_ERROR("サポート外のフォーマットです [name={}, type={}]", name,magic_enum::enum_name(format));
+            return false;
+        }
+
+        return true;
+    }
+
+}
